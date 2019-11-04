3bdeec77d3d1975768a49ed1b74f860054ed133b
GarvTambi <42083679+GarvTambi@users.noreply.github.com>
2019-11-04 23:39:41 +0530

    python3 rotate.py  --image ../opencv/cat.jpeg
    rotate.py code is used to rotate the image by an angle.

---
+++ rotate.py
@@ -0,0 +1,20 @@
+# imutil library  defines in imutil.py
+
+import argparse
+import imutil
+import cv2
+
+# fetching the arguments and save in dictionary
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True, help = "Enter path to the image")
+args = vars(ap.parse_args())
+
+# loading and converting the image into numpy array
+image = cv2.imread(args["image"])
+
+# rotate the image
+rotated = imutil.rotate(image, 180)
+cv2.imshow("180 rotated", rotated)
+cv2.waitKey(0)
+
+
